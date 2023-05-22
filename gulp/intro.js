ca37d72f16a418cbbfaa911f3ff892662125b6b1
Miro Storm <mirostorm@gmail.com>
2023-05-22 12:21:10 +0200

    Add gulp intro

---
+++ gulp/intro.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { task } = require('gulp');
+
+task('intro', (d) => {
+   console.log('█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█');
+   console.log('█ █▀▀▀ █  █ █   █▀▀█  ▀█▀ █▀▀█ █▀▀▀ █ ▄▀ █▀▀▀ █');
+   console.log('█ █ ▀█ █  █ █   █▀▀    █  █▀▀█ ▀▀▀█ █▀▀▄ ▀▀▀█ █');
+   console.log('█ ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀      ▀  ▀  ▀ ▀▀▀▀ ▀  ▀ ▀▀▀▀ █');
+   console.log('█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█');
+   console.log('█        ⚡   WELCOME TO GULP CLI   ⚡        █');
+   console.log('█    by Miro Storm Polfliet    @raremiroir    █');
+   console.log('█---------------------------------------------█');
+   console.log('█               COMMANDS (NPM):               █');
+   console.log('█ 🤖 npm run dev                   dev server █');
+   console.log('█ 🧱 npm run build                build files █');
+   console.log('█ 🫧  npm run clean      clean dev/build files █');
+   console.log('█ 👀 npm run preview      preview build files █');
+   console.log('█---------------------------------------------█');
+   console.log('█               COMMANDS (GULP):              █');
+   console.log('█ 🤖 gulp dev                      dev server █');
+   console.log('█ 🧱 gulp build                   build files █');
+   console.log('█ 🫧  gulp clean         clean dev/build files █');
+   console.log('█ 👀 gulp preview         preview build files █');
+   console.log('█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█');
+   d();
+})
