d3662d3e0d1056d162e5665c176e3cd8ff3dbac3
Miro Storm <mirostorm@gmail.com>
2023-05-22 13:45:03 +0200

    Add done function

--- gulp/preview/reloadPreview.js
+++ gulp/preview/reloadPreview.js
@@ -3,13 +3,13 @@
 const { task } = require('gulp');
 const browserSync = require('browser-sync').create();
 
-task('preview:reload', async (d) => {
+task('preview:reload', (d) => {
+   console.log('----------------------------------------');
    console.log('Reloading...');
    browserSync.reload();
-   d();
    console.log('----------------------------------------');
    console.log('🤖 DEV:');
    console.log('👀 Live preview reloaded!');
-   console.log(`👀 http://localhost:3000 to see dev preview!`);
    console.log('----------------------------------------');
+   d();
 })
