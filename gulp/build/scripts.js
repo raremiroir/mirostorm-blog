761998fc3c31e6e6f0ebf53988e2340dc8ccde00
Miro Storm <mirostorm@gmail.com>
2023-05-23 02:33:23 +0200

    Add script for parsing vendor files

--- gulp/build/scripts.js
+++ gulp/build/scripts.js
@@ -5,11 +5,21 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-terser');
 
 task('build:js', (d) => {
-   src('src/assets/js/**/*.js')
-      .pipe(concat({ path: 'app.js' }))
-      .pipe(uglify())
-      .pipe(dest('./build/'));
-   d();
+   // Main js files
+   src('src/assets/js/**/*.js')           // Get all js files
+      .pipe(concat({ path: 'app.js' }))   // Concat all js files
+      .pipe(uglify())                     // Minify js
+      .pipe(dest('./build/'));            // Output to dist folder
+
+   // Node modules
+   src([                                  // Get required modules
+      'node_modules/iconify-icon/dist/iconify-icon.min.js',
+   ])
+      .pipe(concat({ path: 'vendor.js' }))// Concat all js files
+      .pipe(uglify())                     // Minify js
+      .pipe(dest('./build/'));            // Output to dist folder
+      
+   d();                                   // Done
    
    console.log('----------------------------------------');
    console.log('🧱 BUILD:');
--- gulp/dev/scripts.js
+++ gulp/dev/scripts.js
@@ -5,9 +5,18 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-terser');
 
 task('dev:js', (d) => {
+   // Main js files
    src(`src/assets/js/**/*.js`)              // Get all js files
       .pipe(concat({ path: 'app.js' }))      // Concat all js files
-      .pipe(dest(`public/assets/`));  // Output to dist folder
+      .pipe(dest(`public/assets/`));         // Output to dist folder
+
+   // Node modules
+   src([                                     // Get required modules
+      'node_modules/iconify-icon/dist/iconify-icon.min.js',
+   ])
+      .pipe(concat({ path: 'vendor.js' }))   // Concat all js files
+      .pipe(dest(`public/`));                // Output to dist folder
+
    d();                                      // Done
    
    console.log('----------------------------------------');
