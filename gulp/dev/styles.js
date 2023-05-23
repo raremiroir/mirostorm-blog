41a5cb76e4dd17188607db3bafc94d3a18f2cdab
Miro Storm <mirostorm@gmail.com>
2023-05-23 15:33:52 +0200

    Get sass files + purge css

--- gulp/dev/styles.js
+++ gulp/dev/styles.js
@@ -4,14 +4,21 @@ const { src, dest, task } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const concat = require('gulp-concat');
+const purgecss = require('gulp-purgecss');
 
 task('dev:css', (d) => {
-   const tailwindcss = require('tailwindcss');
-   const autoprefixer = require('autoprefixer');
-   src(`src/assets/css/**/*.scss`)                       // Get all scss files
+   // const tailwindcss = require('tailwindcss');
+   // const autoprefixer = require('autoprefixer');
+   src([                                                 // Get all scss files
+      `src/assets/css/*.scss`,                              // Include scss files
+      `src/assets/css/**/*.scss`,
+      `!src/assets/css/_*.scss`,                            // Exclude files starting with _
+      `!src/assets/css/**/_*.scss`,
+   ])
       .pipe(sass().on('error', sass.logError))           // Parse sass
-      .pipe(postcss([tailwindcss(), autoprefixer()]))    // Parse tailwindcss + autoprefixer
+      // .pipe(postcss([tailwindcss(), autoprefixer()]))    // Parse tailwindcss + autoprefixer
       .pipe(concat({ path: 'app.css' }))                 // Concat all css files
+      .pipe(purgecss({content: [`src/**/*.html`]}))      // Remove unused css
       .pipe(dest(`public/assets/`));                     // Output to dist folder
    d();                                                  // Done
    
