d911a7bdadcc0d2f0da5db75eb5745d45e48eb0c
Miro Storm <mirostorm@gmail.com>
2023-05-24 18:01:32 +0200

    Change hero height depending on images

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,3 +2,19 @@
 window.addEventListener('DOMContentLoaded', (event) => {
    console.log('DOM fully loaded and parsed')
 })
+
+// Hero height
+const defineHeroHeight = () => {
+   const heroImgGrid = document.querySelector('.hero__media--grid');
+   const heroImgGridHeight = heroImgGrid.offsetHeight;
+   console.log('heroImgGridHeight:', heroImgGridHeight);
+
+   const heroMedia = document.querySelector('.hero__media');
+   heroMedia.style.height = (heroImgGridHeight * 1.1) + 'px';
+
+   return;
+}
+defineHeroHeight();
+window.addEventListener('resize', () => {
+   defineHeroHeight();
+});
