4e9734ba7ad307a181efd6b6c05ac4d2a821b588
Miro Storm <mirostorm@gmail.com>
2023-05-25 23:46:16 +0200

    Pipe autoprefixer + remove comments

--- gulp/build/styles.js
+++ gulp/build/styles.js
@@ -2,6 +2,8 @@
 
 const { src, dest, task } = require('gulp');
 const concat = require('gulp-concat');
+const stripCssComments = require('gulp-strip-css-comments');
+const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const config = require('../../gulp-config');
 
@@ -19,8 +21,10 @@ task('build:css', (d) => {
       `${config.paths.dist}/assets/global.css`, // Include css files
    ])
       .pipe(concat({ path: 'app.min.css' }))        // Concat all css files
-      .pipe(cleanCSS())                         // Minify css
-      .pipe(dest(`${config.paths.build}/`))     // Output to build folder
+      .pipe(stripCssComments({ preserve: false }))  // Remove comments
+      .pipe(autoprefixer({ cascade: false }))       // Add vendor prefixes
+      .pipe(cleanCSS())                             // Minify css
+      .pipe(dest(`${config.paths.build}/`))         // Output to build folder
       .on('end', () => {
          console.log(`
             ----------------------------------------
